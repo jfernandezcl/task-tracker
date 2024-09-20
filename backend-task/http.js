d64e65225d613de421097c0c0b1191a82cb31888
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-20 15:33:47 +0200

    updated file

--- backend-task/http.js
+++ backend-task/http.js
@@ -2,9 +2,11 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
+const processRequest = ((req, res) => {
   res.end('Hola mundo')
 })
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
   console.log(`server listening on port http://localhost:${desiredPort}`)

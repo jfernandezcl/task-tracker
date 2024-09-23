d15fd9f7fb8eb9cda6ec95875bae41a5ba921ebc
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-23 11:12:05 +0200

    router getAll created

--- backend-task/routes/taskRoutes.js
+++ backend-task/routes/taskRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { TaskController } from "../controllers/taskControllers";
 
-export const createTaskRouter = ({ }) => {
+export const createTaskRouter = ({ taskModel }) => {
   const taskRouter = Router() // definir rutas y asociarlas con funciones (GET, POST...)
+
+  const newTaskController = new TaskController({ taskModel })
+
+  taskRouter.get('/', newTaskController.getAll)
 
 }

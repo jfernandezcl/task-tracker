cc3e45fb7f293e4fb88d62a1e29ed5d1fcfc033f
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-23 16:14:58 +0200

    configured the server

---
+++ backend-task/app.js
@@ -0,0 +1,19 @@
+import express, { json } from 'express'
+import { createTaskRouter } from './routes/taskRoutes.js'
+import { corsMiddlewares } from './middlewares/corsMiddlewares.js'
+
+export const createApp = ({ taskModel }) => {
+  const app = express()
+
+  app.use(json())
+  app.use(corsMiddlewares())
+  app.disable('x-powered-by')
+
+  app.use('/task', createTaskRouter({ taskModel }))
+}
+
+const desiredPort = process.env.PORT ?? 1234
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})

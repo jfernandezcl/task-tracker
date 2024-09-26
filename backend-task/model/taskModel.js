a5bb016746bf5d259fb10d21fb3a7e78dbf72f65
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-26 11:15:38 +0200

    updated file

--- backend-task/model/taskModel.js
+++ backend-task/model/taskModel.js
@@ -1,9 +1,23 @@
 import mysql from 'mysql2/promise'
 
+// PAra hacer la conexión
 const SERVER_CONFIG = {
   host: 'localhost',
   user: 'root',
   port: 1234,
   password: '',
   database: 'tasktracker'
+}
+
+const connectionString = process.env.DATABASE_URL ?? SERVER_CONFIG
+
+const connection = await mysql.createConnection(connectionString)
+
+// Métodos de la clase
+export class TaskModel {
+  static async getAll() { }
+
+  static async created({ input }) { }
+
+  static async delete({ id }) { }
 }

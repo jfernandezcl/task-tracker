77ce04efde15f8ffd028cd2fe1f954ba4c19dfd1
jfernandezcl <javierfernandez0987@gmail.com>
2024-09-30 17:50:24 +0200

    created the petition

--- frontend-task/src/components/TaskList.jsx
+++ frontend-task/src/components/TaskList.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskForm from './TaskForm.jsx'
 import TaskItem from './TaskItem.jsx';
 
 
 function TaskList() {
   const [displayLists, setDisplayLists] = useState([]);
+
+  useEffect(() => {
+    // Hacer la petición GET para cargar las tareas del backend
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/tasks')
+        const tasks = await response.json()
+        setDisplayLists(tasks)
+      } catch (error) {
+        console.error('Error when loading tasks:', error)
+      }
+    }
+    fetchTasks()
+  }, []);
 
   // Función para agregar una tarea a la lista
   const addTask = (newTask) => {

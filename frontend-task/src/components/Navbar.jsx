e48afc92ac8510a6c6b131f07bd7aacb55fe9a62
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-17 15:34:17 +0200

    feat: created the function of the logout button

--- frontend-task/src/components/Navbar.jsx
+++ frontend-task/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
+import { useLocation } from 'wouter'
 import '../styles/Navbar.css'
 
 function Navbar() {
+
+  const [, navigate] = useLocation()
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    navigate('/login')
+  }
+
   return (
     <>
       <div className='container-navbar'>
-        <button className="button-logout">Log out</button>
+        <button className="button-logout" onClick={handleLogout}>Log out</button>
       </div>
     </>
   )

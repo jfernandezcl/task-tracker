ebee7229a4b0d59dca32d8b18c7b499e113c457c
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-18 15:45:38 +0200

    feat: created the template for the username and password

--- backend-task/model/usersModel.js
+++ backend-task/model/usersModel.js
@@ -1,5 +1,27 @@
 import connection from '../dataBase/dataBase.js'
 
 export class UsersModel {
+  static async create({ username, password }) {
+    // verificar nombre de usuario
+    const [existingUser] = await connection.query(
+      'SELECT * FROM users WHERE username = ?',
+      [username]
+    )
+    if (existingUser.length > 0) {
+      throw new Error('The username already exists, choose another one.')
+    }
+
+    const [result] = await connection.query(
+      'INSERT INTO users (username, password) VALUES (?, ?'
+      [username, password]
+    )
+
+    const [newUser] = await connection.query(
+      'SELECT id, username FROM users WHERE id = ?;'
+      [result.insertId]
+    )
+    return newUser[0]
+  }
+
 
 }

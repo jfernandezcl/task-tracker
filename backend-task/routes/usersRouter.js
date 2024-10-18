10e2d59e7bfdfe405bfb9c5a1f017d401a5f8bc5
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-18 13:19:00 +0200

    fix: name changes for routes

--- backend-task/routes/usersRouter.js
+++ backend-task/routes/usersRouter.js
@@ -6,8 +6,8 @@ export const usersRouter = ({ usersModel }) => {
 
   const usersController = new UsersController({ usersModel })
 
-  usersRouter.get('/login', usersController.getAll)
-  usersRouter.post('/register', usersController.create)
+  myUsersRouter.get('/login', usersController.getAll)
+  myUsersRouter.post('/register', usersController.create)
 
-  return usersRouter
+  return myUsersRouter
 } 

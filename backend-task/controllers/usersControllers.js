6192dee062730141b0fea8aff8779d5df3834b8c
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-19 16:08:32 +0200

    feat: created the method to register the user

--- backend-task/controllers/usersControllers.js
+++ backend-task/controllers/usersControllers.js
@@ -2,4 +2,15 @@ export class usersControllers {
   constructor({ usersModel }) {
     this.usersModel = usersModel
   }
+
+  //metodo para regitrar un nuevo usuario
+  async create(req, res) {
+    try {
+      const user = await this.usersModel.create(req.body)
+      res.status(201).json(user) // devolver el nuevo usuario creado
+    } catch (error) {
+      res.status(400).json({ error: error.message }) // devolver el mensaje de error
+    }
+  }
+
 }

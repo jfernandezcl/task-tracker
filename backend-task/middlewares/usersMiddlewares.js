d90fc114b34e11bc6110abafaef0e1060bd8601d
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 18:49:03 +0200

    feat: created authentication for the user

--- backend-task/middlewares/usersMiddlewares.js
+++ backend-task/middlewares/usersMiddlewares.js
@@ -1,3 +1,21 @@
+import jwt from 'jsonwebtoken'
+
 const jwtSecret = process.env.JWT_SECRET
 
-const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: '1h' })
+// Middleware para verificar el token
+export const authenticateToken = (req, res, next) => {
+  // obtener el token de las cookies o el encabezado
+  const token = req.cookies.token || req.headers['authorization']?.split(' ')[1]
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied, no token provided.' })
+  }
+
+  jwt.verify(token, jwtSecret, (error, user) => {
+    if (error) {
+      return res.status(403).json({ error: 'Invalid token.' })
+    }
+    req.user = user // guardar el usuario en la solicitud
+    next()
+  })
+}

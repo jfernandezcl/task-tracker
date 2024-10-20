37e107e83f61ca0433d88ee28f1fe34ad91602a5
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 20:55:36 +0200

    feat: modify routes for authentication

--- backend-task/routes/taskRoutes.js
+++ backend-task/routes/taskRoutes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { TaskControllers } from "../controllers/taskControllers.js";
+import { authenticateToken } from "../middlewares/usersMiddlewares.js";
+
 
 export const taskRouter = ({ taskModel }) => {
   const taskRouter = Router() // definir rutas y asociarlas con funciones (GET, POST...)
 
   const taskController = new TaskControllers({ taskModel })
 
-  taskRouter.get('/', taskController.getAll)
-  taskRouter.post('/', taskController.create)
-  taskRouter.delete('/:id', taskController.delete)
-  taskRouter.patch('/:id/completed', taskController.updateTaskCompleted)
+  taskRouter.get('/', authenticateToken, taskController.getAll)
+  taskRouter.post('/', authenticateToken, taskController.create)
+  taskRouter.delete('/:id', authenticateToken, taskController.delete)
+  taskRouter.patch('/:id/completed', authenticateToken, taskController.updateTaskCompleted)
 
   return taskRouter
 }

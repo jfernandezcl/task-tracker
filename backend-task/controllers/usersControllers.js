dd4a9238ebf7680fe5d1575207c83e10b20c7f00
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 13:31:31 +0200

    updated file

--- backend-task/controllers/usersControllers.js
+++ backend-task/controllers/usersControllers.js
@@ -3,13 +3,23 @@ export class usersControllers {
     this.usersModel = usersModel
   }
 
-  //metodo para regitrar un nuevo usuario
+  //método para regitrar un nuevo usuario
   async create(req, res) {
     try {
       const user = await this.usersModel.create(req.body)
       res.status(201).json(user) // devolver el nuevo usuario creado
     } catch (error) {
       res.status(400).json({ error: error.message }) // devolver el mensaje de error
+    }
+  }
+
+  // método para iniciar sesión
+  async login(req, res) {
+    try {
+      const authenticatedUser = await this.usersModel.login(req.body)
+      res.status(200)
+    } catch (error) {
+
     }
   }
 

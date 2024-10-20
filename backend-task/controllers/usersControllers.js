372ac34cd9e63da913f970f488c0c924396d5d9b
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 20:41:11 +0200

    feat: finish the drivers for user and password

--- backend-task/controllers/usersControllers.js
+++ backend-task/controllers/usersControllers.js
@@ -1,4 +1,7 @@
 import { usersValidations } from '../validationsRegister/usersValidations'
+import jwt from 'jsonwebtoken'
+
+const jwtSecret = process.env.JWT_SECRET
 
 export class UsersControllers {
   constructor({ usersModel }) {
@@ -23,12 +26,27 @@ export class UsersControllers {
   // método para iniciar sesión
   async login(req, res) {
     try {
-      // iniciar sesión y autenticar al usuario
+      // autenticar al usuario
       const authenticatedUser = await this.usersModel.login(req.body)
-      res.status(200).json(authenticatedUser) // devolver el usuario autenticado
+
+      // crear el token
+      const token = jwt.sign({ id: authenticatedUser.id }, jwtSecret, { expiresIn: '1h' })
+
+      // almacenar el token en una cookie
+      res.cookie('token', token, { httpOnly: true, masAge: 36000000 }) // 1 hora
+
     } catch (error) {
-      res.status(401).json({ error: error.message }) // devolver mensaje de error
+      res.status(401).json({ error: error.message })
     }
   }
 
+  // método para cerrar sesión
+  async logout(req, res) {
+    try {
+      res.clearCookie('token') // limpiar cookie del token
+      res.status(200).json('Logged out successfully')
+    } catch (error) {
+      res.status(500).json({ error: 'Error logging out.' })
+    }
+  }
 }

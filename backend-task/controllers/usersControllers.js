32d7c6b7214e07c6f2e262a25b2230780b99197f
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 14:41:54 +0200

    feat: imported the validations into the controller

--- backend-task/controllers/usersControllers.js
+++ backend-task/controllers/usersControllers.js
@@ -1,3 +1,5 @@
+import { usersValidations } from '../validationsRegister/usersValidations'
+
 export class usersControllers {
   constructor({ usersModel }) {
     this.usersModel = usersModel
@@ -6,6 +8,10 @@ export class usersControllers {
   //método para regitrar un nuevo usuario
   async create(req, res) {
     try {
+
+      // Realizar las validaciones del usuario
+      usersValidations(req.body)
+
       const user = await this.usersModel.create(req.body)
       res.status(201).json(user) // devolver el nuevo usuario creado
     } catch (error) {

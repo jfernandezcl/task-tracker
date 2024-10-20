68a28c70ea19ec6238e7994de6c1654eef684365
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-20 18:32:26 +0200

    corrected errors in the structure

--- backend-task/controllers/usersControllers.js
+++ backend-task/controllers/usersControllers.js
@@ -1,6 +1,6 @@
 import { usersValidations } from '../validationsRegister/usersValidations'
 
-export class usersControllers {
+export class UsersControllers {
   constructor({ usersModel }) {
     this.usersModel = usersModel
   }
@@ -12,6 +12,7 @@ export class usersControllers {
       // Realizar las validaciones del usuario
       usersValidations(req.body)
 
+      // crear el nuevo usuario
       const user = await this.usersModel.create(req.body)
       res.status(201).json(user) // devolver el nuevo usuario creado
     } catch (error) {
@@ -22,10 +23,11 @@ export class usersControllers {
   // método para iniciar sesión
   async login(req, res) {
     try {
+      // iniciar sesión y autenticar al usuario
       const authenticatedUser = await this.usersModel.login(req.body)
-      res.status(200).json(authenticatedUser)
+      res.status(200).json(authenticatedUser) // devolver el usuario autenticado
     } catch (error) {
-      res.status(401).json({ error: error.message })
+      res.status(401).json({ error: error.message }) // devolver mensaje de error
     }
   }
 

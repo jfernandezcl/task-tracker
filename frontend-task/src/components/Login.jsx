ffb6fd7fd031beae5cb373406260204720dec786
jfernandezcl <javierfernandez0987@gmail.com>
2024-10-21 16:43:27 +0200

    feat: configured the buttons for name and password

--- frontend-task/src/components/Login.jsx
+++ frontend-task/src/components/Login.jsx
@@ -1,18 +1,42 @@
+import { useState } from 'react'
 import '../styles/Login-Register.css'
 import { useLocation } from 'wouter'
 
 function Login() {
-
   const [, navigate] = useLocation()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleRegisterClick = (e) => {
     e.preventDefault()
     navigate('/register')
   }
 
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password })
+      })
+
+      if (response.ok) {
+        const { token } = await response.json()
+        localStorage.setItem('token', token) // almacenar el token
+        navigate('/') // redirigir al usuario después de iniciar sesión
+      }
+
+    } catch (error) {
+      console.error('Error in the login request:', error);
+    }
+  }
+
   const handleLoginClick = (e) => {
     e.preventDefault()
-    navigate('/')
+    handleLogin(e)
   }
 
   return (
@@ -26,12 +50,24 @@ function Login() {
         <label className='title-label'>Username</label>
 
         <div className='input-name'>
-          <input className='input-form' type="text" placeholder="Enter your name" />
+          <input
+            className='input-form'
+            type="text"
+            placeholder="Enter your name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
 
         <label className='title-label'>Password</label>
         <div>
-          <input className='input-form' type="password" placeholder="Enter your password" />
+          <input
+            className='input-form'
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div className='container-buttons'>

8a62fa727a5c48e4ba4294cb4b79f09223a29830
jfernandecl <javierfernandez0987@gmail.com>
2025-05-19 12:19:24 +0200

    feat: update routing structure and dependencies; add AppRoutes component

--- frontend-task/src/App.jsx
+++ frontend-task/src/App.jsx
@@ -1,18 +1,8 @@
-import './App.css';
-import Login from './components/Login.jsx';
-import Register from './components/Register.jsx';
-import TaskList from './components/TaskList.jsx';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<TaskList />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-
-  );
+  return <AppRoutes />;
 }
 
 export default App;

f7a53aa7a569b6b95261f390b65fc73bbe3be65e
jfernandecl <javierfernandez0987@gmail.com>
2025-07-21 13:06:23 +0200

    chore: configure CORS to allow PATCH from the frontend

--- backend-task/middlewares/corsMiddlewares.js
+++ backend-task/middlewares/corsMiddlewares.js
@@ -1,10 +1,10 @@
-import cors from 'cors'
+import cors from "cors";
 
 export const corsMiddlewares = () => {
   return cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  })
-}
+  });
+};
